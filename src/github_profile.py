0cd489313de3c8855b4e763fd34bb55f222fd076
Ali Mogharrab <alimogharrab101@gmail.com>
2023-01-25 16:28:36 +0330

    Change generate_profile function to GenerateProfile class

--- src/github_profile.py
+++ src/github_profile.py
@@ -1,53 +1,39 @@
 from pathlib import Path
 
 
-def generate_profile(theme, **kwargs):
-    """Generate profile readme from theme and kwargs"""
-    # Read theme profile
-    with open(f"src/themes/{theme}/profile.txt") as f:
-        profile = f.read()
+class GenerateProfile:
 
-    # Replace placeholders with items
-    for item, value in kwargs.items():
-        # Skip empty values
-        if not value:
-            profile = profile.replace(f"{{ {item} }}", '')
-            continue
+    def __init__(self, theme, **kwargs):
+        self.theme = theme
+        self.kwargs = kwargs
+        self.profile_theme = self.read_profile_theme()
 
-        # Skip items that don't exist in theme
-        item_path = Path(f"src/themes/{theme}/{item}.txt")
-        if not item_path.exists():
-            continue
-
-        # Replace placeholder with item
-        with open(item_path) as f:
-            profile_item = f.read().strip()
-        profile_item = profile_item.replace("{ value }", value)
-        profile = profile.replace(f"{{ {item} }}", profile_item)
-
-    return profile
+    def read_profile_theme(self):
+        """Generate profile readme from theme and kwargs"""
+        # Read theme profile
+        with open(f"src/themes/{self.theme}/profile.txt") as f:
+            profile = f.read()
+        return profile
 
 
-if __name__ == '__main__':
-    # Personal Info
-    name = "John Doe"
-    email = "johndoe@gmail.com"
-    phone = "+1 123 456 7890"
-    homepage = "https://johndoe.com"
-    location = "New York, USA"
+    def __call__(self):
+        # Replace placeholders with items
+        for item, value in self.kwargs.items():
+            # Skip empty values
+            if not value:
+                self.profile_theme = self.profile_theme.replace(f"{{ {item} }}", '')
+                continue
 
-    # Social Media
-    github = "johndoe"
-    linkedin = "johndoe"
-    twitter = "johndoe"
-    facebook = "johndoe"
-    instagram = "johndoe"
-    youtube = "johndoe"
-    medium = "johndoe"
+            # Skip items that don't exist in theme
+            item_path = Path(f"src/themes/{self.theme}/{item}.txt")
+            if not item_path.exists():
+                continue
 
-    # Select Theme
-    theme = "default"
+            # Replace placeholder with item
+            with open(item_path) as f:
+                profile_item = f.read().strip()
+            profile_item = profile_item.replace("{ value }", value)
+            self.profile_theme = self.profile_theme.replace(f"{{ {item} }}", profile_item)
 
-    # Generate Readme
-    profile = generate_profile(theme, name=name, email=email)
-    print(profile)
+        return self.profile_theme
+        
